b83998ba263c42b674eab3b4a53b978eb3a6bfcf
Bilal Abu-Hashem <Bilal@imc-grp.com>
2023-04-24 11:28:59 +0300

    24.04.23

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,14 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
 
-
-
 @NgModule({
-  declarations: [
-    ProductComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [ProductComponent],
+  imports: [CommonModule],
 })
-export class ProductModule { }
+export class ProductModule {}
--- src/app/product/services/product.service.ts
+++ src/app/product/services/product.service.ts
@@ -4,6 +4,7 @@ import {
   BehaviorSubject,
   debounceTime,
   delay,
+  map,
   Observable,
   of,
   Subject,
@@ -12,6 +13,14 @@ import { Routes } from 'src/app/core/http/API';
 import { StorageService } from 'src/app/core/services/storage.service';
 import { IProduct } from 'src/app/shared/models';
 import { PRODUCTS_MOCK } from './products.mock';
+
+import * as uuid from 'uuid';
+import { Store } from '@ngxs/store';
+import {
+  DeleteCartItem,
+  SetCartItem,
+} from 'src/app/core/state/product/product.actions';
+import { ProductStateSelectors } from 'src/app/core/state/product/product.selectors';
 
 @Injectable({
   providedIn: 'root',
@@ -23,7 +32,8 @@ export class ProductService {
 
   constructor(
     private http: HttpClient,
-    private storageService: StorageService
+    private storageService: StorageService,
+    private store: Store
   ) {}
 
   private productsSubject$: BehaviorSubject<IProduct[]> = new BehaviorSubject(
@@ -36,7 +46,7 @@ export class ProductService {
 
   public fetchProducts(): void {
     const existingData: IProduct[] = this.storageService.getData('products');
-
+    debugger;
     if (existingData) {
       this.productsSubject$.next(existingData);
     } else {
@@ -85,5 +95,36 @@ export class ProductService {
     this.storageService.setData('products', productsList);
 
     this.fetchProducts();
+  }
+
+  public addNewProduct(result: IProduct): void {
+    const productsList = this.productsSubject$.value;
+
+    result.id = uuid.v4(); // productsList.length + 1; // OR generate random id
+
+    debugger;
+    productsList.push(result);
+    this.storageService.setData('products', productsList);
+
+    this.fetchProducts();
+  }
+
+  public addToCart(product: IProduct): void {
+    debugger;
+    this.store.dispatch(new SetCartItem(product));
+  }
+
+  public isProductInCart$(productId: number): Observable<boolean> {
+    return this.store.select(ProductStateSelectors.cartItems).pipe(
+      map((data) => {
+        const isInCart = data.some((Item) => Item.id === productId);
+
+        return isInCart;
+      })
+    );
+  }
+
+  removeFromCart(id: number) {
+    this.store.dispatch(new DeleteCartItem(id));
   }
 }
